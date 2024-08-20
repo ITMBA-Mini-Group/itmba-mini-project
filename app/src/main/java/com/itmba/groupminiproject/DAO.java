cc0e37cb768d3b34f0778d6ebac1637edde9b8a2
MrTea <111572394+LilStevieDev@users.noreply.github.com>
2024-08-20 10:44:48 +0200

    Added utility functions

--- app/src/main/java/com/itmba/groupminiproject/DAO.java
+++ app/src/main/java/com/itmba/groupminiproject/DAO.java
@@ -1,31 +1,60 @@
 package com.itmba.groupminiproject;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DAO extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME="Student.db";
-    private static final int DATABASE_VERSION=1;
+private static final String DATABASE_NAME="Student.db";
+private static final int DATABASE_VERSION=1;
 
-    public static final String TABLE_NAME= "Students";
+public static final String TABLE_NAME= "Students";
 
-    public static final String  COLUMN_NUMBER= "Student_Number";
-    public static final String COLUMN_NAME = "Student_Name";
-    public static final String COLUMN_SURNAME= "Student_Surname";
-    public static final String COLUMN_PHONE= "Student_Phone";
+public static final String  COLUMN_NUMBER= "Student_Number";
+public static final String COLUMN_NAME = "Student_Name";
+public static final String COLUMN_SURNAME= "Student_Surname";
+public static final String COLUMN_PHONE= "Student_Phone";
 
     private static final String TABLE_CREATE =
             "CREATE TABLE " + TABLE_NAME + " (" +
-                    COLUMN_NUMBER + " INTEGER PRIMARY KEY, " +
+                    COLUMN_NUMBER + " TEXT PRIMARY KEY, " +
                     COLUMN_NAME + " TEXT, " +
                     COLUMN_SURNAME + " TEXT, " +
-                    COLUMN_PHONE + " INTEGER);";
+                    COLUMN_PHONE + " TEXT);";
 
     public DAO(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
+public Cursor viewAllStudents(){
+        SQLiteDatabase db =this.getReadableDatabase();
+        String query ="SELECT * FROM "+ TABLE_NAME;
+        return db.rawQuery(query,null);
+}
+
+public Cursor searchStudents(String studentNumber){
+        SQLiteDatabase db =this.getReadableDatabase();
+        String query= "SELECT * FROM "+TABLE_NAME+"WHERE"+COLUMN_NUMBER+"='"+studentNumber+"'";
+        Cursor cursor=db.rawQuery(query,new String[]{studentNumber});
+
+    return cursor;
+}
+public long addStudent(String studentNumber, String studentName, String studentSurname, String studentPhone){
+        SQLiteDatabase db =this.getWritableDatabase();
+    ContentValues values=new ContentValues();
+
+    values.put(COLUMN_NUMBER, studentNumber);
+    values.put(COLUMN_NAME, studentName);
+    values.put(COLUMN_SURNAME, studentSurname);
+    values.put(COLUMN_PHONE, studentPhone);
+
+
+
+    return db.insert(TABLE_NAME,null,values);
+}
 
 
 
@@ -42,3 +71,4 @@ public class DAO extends SQLiteOpenHelper {
         onCreate(db);
     }
 }
+ 
